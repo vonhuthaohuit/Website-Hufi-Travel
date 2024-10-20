dd9583eebaec5f2e5698abdd96be5e1c5c524665
vohoankien <hoankien2k3@gmai.com>
2024-10-20 22:30:54 +0700

    Update migra

--- database/migrations/2024_10_17_094554_create_chuongtrinhtour_table.php
+++ database/migrations/2024_10_17_094554_create_chuongtrinhtour_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('chuongtrinhtour', function (Blueprint $table) {
             $table->increments('id');
             $table->string('tieude');
+            $table->string('ngay');
             $table->date('thoigianbatdau') ;
             $table->date('thoigianketthuc') ;
             $table->string('mota');
