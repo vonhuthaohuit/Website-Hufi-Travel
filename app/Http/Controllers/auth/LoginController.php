78d9e91bfe1126ccbd79147574eae3445c908a7f
vonhuthaohuit <vonhuthao112345@gmail.com>
2024-11-07 22:00:27 +0700

    Update code dat tour

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -13,107 +13,107 @@ use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
-    public function index() {
-        return view('auth.login');
-    }
+    // public function index() {
+    //     return view('auth.login');
+    // }
 
-    public function getGoogleSignInUrl()
-    {
-       return Socialite::driver('google')->redirect();
-    }
-    public function loginCallback()
-    {
+    // public function getGoogleSignInUrl()
+    // {
+    //    return Socialite::driver('google')->redirect();
+    // }
+    // public function loginCallback()
+    // {
 
-        try {
-            $user = Socialite::driver('google')->user();
-            $finduser = User::where('google_id', $user->id)->first();
-            if($finduser)
-            {
-                Auth::login($finduser);
-                Session::put('user', $finduser);
-                return redirect()->route('home')->with('success', 'Đăng nhập thành công');
-            }
-            else
-            {
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'google_id'=> $user->id,
-                    'nhomquyen_id'=>'1',
-                    'status'=>'Hoạt động',
-                    'password' => encrypt('123456dummy')
-                ]);
-                Auth::login($newUser);
-                Session::put('user', $newUser);
-                return redirect()->route('home')->with('success', 'Đăng nhập thành công');
+    //     try {
+    //         $user = Socialite::driver('google')->user();
+    //         $finduser = User::where('google_id', $user->id)->first();
+    //         if($finduser)
+    //         {
+    //             Auth::login($finduser);
+    //             Session::put('user', $finduser);
+    //             return redirect()->route('home')->with('success', 'Đăng nhập thành công');
+    //         }
+    //         else
+    //         {
+    //             $newUser = User::create([
+    //                 'name' => $user->name,
+    //                 'email' => $user->email,
+    //                 'google_id'=> $user->id,
+    //                 'nhomquyen_id'=>'1',
+    //                 'status'=>'Hoạt động',
+    //                 'password' => encrypt('123456dummy')
+    //             ]);
+    //             Auth::login($newUser);
+    //             Session::put('user', $newUser);
+    //             return redirect()->route('home')->with('success', 'Đăng nhập thành công');
 
-            }
-
-
-        } catch (Exception $e)
-        {
-            dd($e->getMessage());
-        }
-
-    }
-
-    public function register(Request $request)
-    {
-        try {
-            $validator = Validator::make($request->all(), [
-                'name' => 'required|string',
-                'password' => 'required|string',
-                'email' => 'required|email|unique:users,email',
-            ]);
+    //         }
 
 
-            if ($validator->fails()) {
-                return redirect()->back()->withErrors($validator)->withInput();
-            }
+    //     } catch (Exception $e)
+    //     {
+    //         dd($e->getMessage());
+    //     }
 
-            $user = new User();
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->password = $request->password; // Hash password
-            $user->nhomquyen_id = 1;
-            $user->status = "Hoạt động";
-            $user->save();
-            Auth::login($user); // Login user after registration
-            Session::put('success','Đăng kí thành công' );
-            return redirect()->route('login_view');
-        } catch (\Throwable $th) {
-            return redirect()->back()->with('error', $th->getMessage());
-        }
-    }
+    // }
 
-    public function login(Request $request)
-    {
-        try {
-            if (Auth::attempt(['email'=> $request->email_login,'password' =>$request->password_login ])) {
-                $request->session()->regenerate(); // Regenerate session for security
-                $user =  $request->user() ;
-                Session::put('user',$user );
-                return redirect()->route('home');
-            }
-            return back()->withErrors([
-                'message' => 'Thông tin đăng nhập không chính xác.',
-            ])->withInput();
-        } catch (\Throwable $th) {
-            return redirect()->back()->with('error', $th->getMessage());
-        }
-    }
+    // public function register(Request $request)
+    // {
+    //     try {
+    //         $validator = Validator::make($request->all(), [
+    //             'name' => 'required|string',
+    //             'password' => 'required|string',
+    //             'email' => 'required|email|unique:users,email',
+    //         ]);
 
 
-    public function logout(Request $request)
-    {
-        try {
-            Auth::logout(); // Log the user out
-            $request->session()->invalidate(); // Invalidate session
-            $request->session()->regenerateToken(); // Regenerate CSRF token
-            Session::put('success','Đăng xuất thành công' );
-            return redirect()->route('login_view');
-        } catch (\Throwable $th) {
-            return redirect()->back()->with('error', $th->getMessage());
-        }
-    }
+    //         if ($validator->fails()) {
+    //             return redirect()->back()->withErrors($validator)->withInput();
+    //         }
+
+    //         $user = new User();
+    //         $user->name = $request->name;
+    //         $user->email = $request->email;
+    //         $user->password = $request->password; // Hash password
+    //         $user->nhomquyen_id = 1;
+    //         $user->status = "Hoạt động";
+    //         $user->save();
+    //         Auth::login($user); // Login user after registration
+    //         Session::put('success','Đăng kí thành công' );
+    //         return redirect()->route('login_view');
+    //     } catch (\Throwable $th) {
+    //         return redirect()->back()->with('error', $th->getMessage());
+    //     }
+    // }
+
+    // public function login(Request $request)
+    // {
+    //     try {
+    //         if (Auth::attempt(['email'=> $request->email_login,'password' =>$request->password_login ])) {
+    //             $request->session()->regenerate(); // Regenerate session for security
+    //             $user =  $request->user() ;
+    //             Session::put('user',$user );
+    //             return redirect()->route('home');
+    //         }
+    //         return back()->withErrors([
+    //             'message' => 'Thông tin đăng nhập không chính xác.',
+    //         ])->withInput();
+    //     } catch (\Throwable $th) {
+    //         return redirect()->back()->with('error', $th->getMessage());
+    //     }
+    // }
+
+
+    // public function logout(Request $request)
+    // {
+    //     try {
+    //         Auth::logout(); // Log the user out
+    //         $request->session()->invalidate(); // Invalidate session
+    //         $request->session()->regenerateToken(); // Regenerate CSRF token
+    //         Session::put('success','Đăng xuất thành công' );
+    //         return redirect()->route('login_view');
+    //     } catch (\Throwable $th) {
+    //         return redirect()->back()->with('error', $th->getMessage());
+    //     }
+    // }
 }
