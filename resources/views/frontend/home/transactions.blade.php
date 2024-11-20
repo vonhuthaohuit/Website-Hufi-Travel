c4e8110d01caebf4581bc816c0a4e50581d3a903
VuongTranDev <vuongtran975@gmail.com>
2024-11-20 22:38:21 +0700

    update feature show tour order

--- resources/views/frontend/home/transactions.blade.php
+++ resources/views/frontend/home/transactions.blade.php
@@ -25,22 +25,35 @@
                             stroke-linejoin="round"></path>
                         <path d="M7.5 17V16H8.5V17H7.5Z" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"
                             stroke-linejoin="round"></path>
-                    </svg> Tất cả sản phẩm
+                    </svg> Tất cả giao dịch
                 </button>
 
             </div>
 
             <div class="col-md-8">
                 @if (session('user'))
-                    <div class="transaction-status mt-3">
-                        <img src="https://ik.imagekit.io/tvlk/image/imageResource/2020/07/10/1594367281441-5ec1b573d106b7aec243b19efa02ac56.svg?tr=h-96,q-75,w-96"
-                            alt="No transaction icon">
-                        <div>
-                            <h5>Không có giao dịch đang tiến hành</h5>
-                            <p>Bạn không có giao dịch đang tiến hành nào từ phiên giao dịch trước. Những giao dịch chưa hoàn
-                                thành sẽ được lưu tại đây.</p>
+                    @if (count($toursDaDat) === 0)
+                        <div class="transaction-status mt-3">
+                            <img src="https://ik.imagekit.io/tvlk/image/imageResource/2020/07/10/1594367281441-5ec1b573d106b7aec243b19efa02ac56.svg?tr=h-96,q-75,w-96"
+                                alt="No transaction icon">
+                            <div>
+                                <h5>Không có giao dịch đang tiến hành</h5>
+                                <p>Bạn không có giao dịch đang tiến hành nào từ phiên giao dịch trước. Những giao dịch chưa
+                                    hoàn
+                                    thành sẽ được lưu tại đây.</p>
+                            </div>
                         </div>
-                    </div>
+                    @else
+                        @foreach ($toursDaDat as $item)
+                            <div class="transaction-item mt-3">
+                                <img src="{{ asset($item->hinhdaidien) }}" alt="No transaction icon">
+                                <div>
+                                    <h5>{{ $item->tentour }}</h5>
+                                    <p>Giá: {{ number_format($item->giatour) }}đ - <span style="color: green;">{{ $item->trangthaidattour }}</span></p>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
                 @else
                     <div class="alert alert-primary alert-dismissible fade show" role="alert"
                         style="background-color: rgba(27, 160, 226, 1.00); color: #fff;">
