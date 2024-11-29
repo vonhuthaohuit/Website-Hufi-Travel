0328fd893a94a55d0937a46c53ba3d24a20042a8
vonhuthaohuit <vonhuthao112345@gmail.com>
2024-11-29 10:46:10 +0700

    Update thêm hoá đơn

--- resources/views/frontend/thanhtoan/payment_success.blade.php
+++ resources/views/frontend/thanhtoan/payment_success.blade.php
@@ -1,45 +1,51 @@
 @extends('frontend.layouts.app')
 <link rel="stylesheet" href="{{ asset('frontend/css/style_dattour.css') }}">
 @section('renderBody')
-<div class="content container pt-5">
-    <div class="container-fluid">
-        <div class="panel panel-1">
-        <div class="panel-body">
-        <form name="tourBooking" method="post" class="form-horizontal frm-tour-booking" role="form" action="/">
-            <ul class="nav nav-tabs nav-justified frm-tour-booking-step" role="tablist">
-                <li role="presentation" class="active"><b>Chọn tour</b><i class="fa fa-check"></i></li>
-                <li role="presentation" class="active"><b>Đăng ký tour</b><i class="fa fa-check"></i></li>
-                <li role="presentation" class="active"><b>Xác nhận thông tin</b><i class="fa fa-check"></i></li>
-                <li role="presentation" class="active"><b>Hoàn thành</b><i class="fa fa-check"></i></li>
-            </ul>
+    <div class="content container pt-5">
+        <div class="container-fluid">
+            <div class="panel panel-1">
+                <div class="panel-body">
+                    <form name="tourBooking" method="post" class="form-horizontal frm-tour-booking" role="form"
+                        action="/">
+                        <ul class="nav nav-tabs nav-justified frm-tour-booking-step" role="tablist">
+                            <li role="presentation" class="active"><b>Chọn tour</b><i class="fa fa-check"></i></li>
+                            <li role="presentation" class="active"><b>Đăng ký tour</b><i class="fa fa-check"></i></li>
+                            <li role="presentation" class="active"><b>Xác nhận thông tin</b><i class="fa fa-check"></i></li>
+                            <li role="presentation" class="active"><b>Hoàn thành</b><i class="fa fa-check"></i></li>
+                        </ul>
 
-                <div class="content_book tour-des">
-                    <header class="content-header">
-                        <h3 class="py-3">Đặt tour thành công!</h3>
-                    </header>
-                    <div style="font-size: 40px; text-align:center; margin-top:30px">
-                        <img src="{{ asset('frontend/images/success.png') }}" alt="" width="100" height="100">
-                    </div>
-                    <div style="max-width: 900px; text-align:center; margin:auto; margin-top:30px">
-                        <p>Cám ơn quý khách đã đăng ký tour tại <b>Hufi Travel</b>.
-                        <br>
-                        Click <a class="text-info" href="/">vào đây</a> trở về trang chủ.</p>
-                        <p>Một email xác nhận đã được gửi đến địa chỉ <a href="mailto:info@Hufitravel.com.vn /sales@Hufitravel.com.vn">info@Hufitravel.com.vn /sales@Hufitravel.com.vn</a>.</p>
-                    </div>
-                    <div class="bs-callout bs-callout-danger">
-                    </div>
+                        <div class="content_book tour-des">
+                            <header class="content-header">
+                                <h3 class="py-3">Đặt tour thành công!</h3>
+                            </header>
+                            <div style="font-size: 40px; text-align:center; margin-top:30px">
+                                <img src="{{ asset('frontend/images/success.png') }}" alt="" width="100"
+                                    height="100">
+                            </div>
+                            <div style="max-width: 900px; text-align:center; margin:auto; margin-top:30px">
+                                <p>Cám ơn quý khách đã đăng ký tour tại <b>Hufi Travel</b>.
+                                    <br>
+                                    Click <a class="text-info" href="/">vào đây</a> trở về trang chủ.
+                                </p>
+                                <p>Một email xác nhận đã được gửi đến địa chỉ <a
+                                        href="mailto:info@Hufitravel.com.vn /sales@Hufitravel.com.vn">info@Hufitravel.com.vn
+                                        /sales@Hufitravel.com.vn</a>.</p>
+                            </div>
+                            <div class="bs-callout bs-callout-danger">
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
-            </form>
-
-    </div>
+            </div>
         </div>
     </div>
-</div>
+    @push('script')
+        <script>
+            setTimeout(function() {
+                console.log('Redirecting...');
+                window.location.href = '/';
+            }, 5000);
+        </script>
+    @endpush
 @endsection
-@push('scripts')
-<script>
-    setTimeout(function() {
-        window.location.href = '/';
-    }, 5000);
-</script>
-@endpush
