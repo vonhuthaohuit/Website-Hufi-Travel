e242069e1dcc00f117db3aa68f154f0b34d2b903
VuongTranDev <vuongtran975@gmail.com>
2024-12-17 23:50:32 +0700

    fix cancel tour

--- app/Http/Controllers/frontend/PhieuHuyController.php
+++ app/Http/Controllers/frontend/PhieuHuyController.php
@@ -36,7 +36,7 @@ class PhieuHuyController extends Controller
         if (!$phieuDatTour) {
             return redirect()->back()->with('error', 'Lỗi trong quá trình hủy tour!');
         } else {
-            $phieuDatTour->trangthaidattour = 'Đã hủy';
+            $phieuDatTour->trangthaidattour = 'Yêu cầu hủy tour';
             $phieuDatTour->save();
         }
 
@@ -50,31 +50,31 @@ class PhieuHuyController extends Controller
         HoaDon::where('maphieudattour', $request->maphieudattour)
             ->update(['maphieuhuytour' => $maphieuhuytour]);
 
-        $tour = PhieuDatTour::query()
-            ->join('tour', 'phieudattour.matour', '=', 'tour.matour')
-            ->select('tour.giatour', 'phieudattour.ngaybatdau')
-            ->where('phieudattour.maphieudattour', $request->maphieudattour)
-            ->first();
+        // $tour = PhieuDatTour::query()
+        //     ->join('tour', 'phieudattour.matour', '=', 'tour.matour')
+        //     ->select('tour.giatour', 'phieudattour.ngaybatdau')
+        //     ->where('phieudattour.maphieudattour', $request->maphieudattour)
+        //     ->first();
 
-        if ($tour) {
-            $ngayBatDau = \Carbon\Carbon::parse($tour->ngaybatdau);
-            $ngayHuy = \Carbon\Carbon::parse($phieuhuy->ngayhuy);
+        // if ($tour) {
+        //     $ngayBatDau = \Carbon\Carbon::parse($tour->ngaybatdau);
+        //     $ngayHuy = \Carbon\Carbon::parse($phieuhuy->ngayhuy);
 
-            $soNgayTruocBatDau = $ngayHuy->diffInDays($ngayBatDau, false);
+        //     $soNgayTruocBatDau = $ngayHuy->diffInDays($ngayBatDau, false);
 
-            $phanTramHoanTien = match (true) {
-                $soNgayTruocBatDau > 30 => 1,
-                $soNgayTruocBatDau >= 16 => 0.7,
-                $soNgayTruocBatDau >= 8 => 0.3,
-                $soNgayTruocBatDau >= 4 => 0.1,
-                default => 0.0,
-            };
+        //     $phanTramHoanTien = match (true) {
+        //         $soNgayTruocBatDau > 30 => 1,
+        //         $soNgayTruocBatDau >= 16 => 0.7,
+        //         $soNgayTruocBatDau >= 8 => 0.3,
+        //         $soNgayTruocBatDau >= 4 => 0.1,
+        //         default => 0.0,
+        //     };
 
-            $soTienHoan = $tour->giatour * $phanTramHoanTien;
+        //     $soTienHoan = $tour->giatour * $phanTramHoanTien;
 
-            $phieuhuy->sotienhoan = $soTienHoan;
-            $phieuhuy->save();
-        }
+        //     $phieuhuy->sotienhoan = $soTienHoan;
+        //     $phieuhuy->save();
+        // }
 
         return response(['status' => 'success', 'message' => 'Hủy tour thành công']);
     }
